cb60d089beae17af1ed019ebaa6b9c498dd40c15
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2024-05-26 23:24:10 -0300

    Desafio .Net POO Dio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+
+Smartphone iphone = new Iphone(numero:"1234567", modelo: "modelo xr", imei:"1025",memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Binance");
+iphone.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+
+Smartphone nokia = new Nokia(numero:"1234567", modelo: "modelo nokia lumia", imei:"1030",memoria: 100);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
